ad852e666afd51bcc2f7f7a535986dac702d404f
Andreas O'Rourke <andreasorourke@gmail.com>
2023-11-16 16:10:56 -0500

    added more JS. Ran into issue adjusting is-hidden attribute

--- santore.js
+++ santore.js
@@ -15,14 +15,63 @@ const setSlidePosition = (slide, index) => {
 }
 slides.forEach(setSlidePosition);
 
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current_slide');
+    targetSlide.classList.add('current_slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('currentSlide');
+    targetDot.classList.add('currentSlide');
+
+}
+
 //when I click left, move slides to the left
+prevButton.addEventListener('click', e =>{
+    const currentSlide = track.querySelector('.current_slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector(".currentSlide");
+    const prevDot = currentDot.previousElementSibling;
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+})
+
 //when I click right, move slides to the right
 nextButton.addEventListener('click', e => {
     const currentSlide = track.querySelector('.current_slide');
     const nextSlide = currentSlide.nextElementSibling;
-    const amountToMove = nextSlide.style.left;
-    //move to the next slide
-    track.style.transform = 'translateX(' + amountToMove + ')';
+    const currentDot = dotsNav.querySelector(".currentSlide");
+    const nextDot = currentDot.nextElementSibling;
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
 })
-//when I click the blips, move to that appropriate slide
 
+
+//when I click the blips, move to that appropriate slide
+dotsNav.addEventListener('click', e => {
+    // what blip was clicked on?
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector('.current_slide');
+    const currentDot = dotsNav.querySelector('.currentSlide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+
+    updateDots(currentDot, targetDot);
+
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.length -1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        prevButton.classList.remove('.is-hidden');
+        nextButton.classList.remove('.is-hidden');
+    }
+})
